9ee4861ffaba40b748274d31bba624a8e55c8d0b
david-pessoa <pessoa.dvlbp@gmail.com>
2024-06-11 22:31:09 -0300

    finaliza app

--- Day 31/main.py
+++ Day 31/main.py
@@ -1,29 +1,55 @@
-from email.mime import image
 from tkinter import *
 import pandas as pd
 from random import choice
 
 BACKGROUND_COLOR = "#B1DDC6"
+try:
+    df = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    df = pd.read_csv("data/french_words.csv")
 
-df = pd.read_csv("data/french_words.csv")
 info = df.to_dict(orient= "records")
-
-
+to_learn = []
 current_dict = {}
 
-def choose_french_word():
-    global current_dict, flip_timer
-    window.after_cancel(flip_timer)
-
-    current_dict = choice(info)
+def config_card_front(word):
     canvas.itemconfig(card_img, image= card_front_img)
     canvas.itemconfig(card_title, text= "French", fill= "black")
-    canvas.itemconfig(card_word, text= current_dict["French"], fill = "black")
-    flip_timer = window.after(3000, show_english_translation)
+    canvas.itemconfig(card_word, text= word, fill = "black")
+
+def choose_french_word_right():
+    global current_dict, flip_timer
+    try:
+        current_dict = choice(info)
+    except IndexError:
+        return
+    window.after_cancel(flip_timer)
+    to_learn.append(current_dict)
+    info.remove(current_dict)
+
+    try:
+        current_dict = choice(info)
+        config_card_front(current_dict["French"])
+        flip_timer = window.after(3000, show_english_translation)
+    except IndexError:
+        config_card_front("No more cards")
+
+def choose_french_word_wrong():
+    global current_dict, flip_timer
+    window.after_cancel(flip_timer)
+    try:
+        current_dict = choice(info)
+        config_card_front(current_dict["French"])
+        flip_timer = window.after(3000, show_english_translation)
+    except IndexError:
+        config_card_front("No more cards")
 
 
 def show_english_translation():
     global current_dict
+    for dictionary in to_learn:
+        if(dictionary["English"] == current_dict["English"]):
+            to_learn.remove(dictionary)
     canvas.itemconfig(card_img, image= card_back_img)
     canvas.itemconfig(card_title, text= "English", fill= "white")
     canvas.itemconfig(card_word, text= current_dict["English"], fill = "white")
@@ -47,14 +73,18 @@ card_title = canvas.create_text(400, 150, text= "Title", font= ("Arial", 40, "it
 card_word = canvas.create_text(400, 263, text= "word", font= ("Arial", 60, "bold"))
 
 cross_image = PhotoImage(file= "images/wrong.png")
-unknow_button = Button(image= cross_image, highlightthickness= 0, command= choose_french_word)
+unknow_button = Button(image= cross_image, highlightthickness= 0, command= choose_french_word_wrong)
 unknow_button.grid(row= 1, column= 0)
 
 check_image = PhotoImage(file= "images/right.png")
-check_button = Button(image= check_image, highlightthickness= 0, command= choose_french_word)
+check_button = Button(image= check_image, highlightthickness= 0, command= choose_french_word_right)
 check_button.grid(row= 1, column= 1)
 
-choose_french_word()
-
+choose_french_word_wrong()
 
 window.mainloop()
+
+newdf = pd.DataFrame(to_learn)
+
+newdf.to_csv("data/words_to_learn.csv", index= False)
+
