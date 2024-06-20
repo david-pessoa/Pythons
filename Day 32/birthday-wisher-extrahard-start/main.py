c00f7d7a182f548e537daedb6a2b2c31b0d2e6ba
david-pessoa <pessoa.dvlbp@gmail.com>
2024-06-20 15:35:34 -0300

    finish automated birthday wisher challenge

---
+++ Day 32/birthday-wisher-extrahard-start/main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import smtplib
+from random import choices
+import datetime as dt
+
+MY_EMAIL = "My email"
+PASSWORD = "App password"
+DESTINY = "email_destination"
+today = dt.datetime.now()
+
+df = pd.read_csv("birthdays.csv")
+contacts = df.to_dict(orient= 'records')
+chosen_letter = "./letter_templates/" + choices(["letter_1.txt", "letter_2.txt", "letter_3.txt"])[0]
+
+for row in contacts:
+    letter = ""
+    if today.month == row["month"] and today.day == row["day"]:
+        with open(chosen_letter, 'r') as file:
+            letter = file.readlines()
+            letter = "\n".join(letter)
+            letter = letter.replace("[NAME]", row["name"])
+        
+        with smtplib.SMTP(host= "smtp.gmail.com", port= 587) as connection:
+            connection.starttls()
+            connection.login(user= MY_EMAIL, password= PASSWORD)
+            connection.sendmail(from_addr= MY_EMAIL, to_addrs= DESTINY, msg= f"Subject:Happy Birthday, {row['name']}\n\n{letter}")
+
+
+
+
+
